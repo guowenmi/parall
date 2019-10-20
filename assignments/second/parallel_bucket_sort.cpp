efda9a7af798be0404173e86d0f32fb49e0c61a3
guowenmi <guowm@svocloud.com>
2019-10-20 17:09:16 +1300

    modify

--- assignments/second/parallel_bucket_sort.cpp
+++ assignments/second/parallel_bucket_sort.cpp
@@ -135,13 +135,13 @@ int main(int argc, char **argv)
     int *nitems = (int*)calloc(buckets_number, sizeof(int));
     unsigned long step = number_size/processes_number;
 
-//    for (int i = 0; i < curr_proc_data_size; i++)
-//    {
-//        int bktno = floor(curr_proc_data[i]/step);// in which small bucket
-//        int idx = bktno * curr_proc_data_size + nitems[bktno];// index in the bucket
-//        bucket[idx] = curr_proc_data[i];
-//        ++nitems[bktno];
-//    }
+    for (int i = 0; i < curr_proc_data_size; i++)
+    {
+        int bktno = floor(curr_proc_data[i]/step);// in which small bucket
+        int idx = bktno * curr_proc_data_size + nitems[bktno];// index in the bucket
+        bucket[idx] = curr_proc_data[i];
+        ++nitems[bktno];
+    }
 
     // step 4, each processor scatter its numbers to proper processors and gather its own proper numbers from others
     // firstly, need to let all processores know how many numbers should recv from each processor
