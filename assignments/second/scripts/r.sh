3087c0288d06da2e72d9cfe12d4196dd59718f7d
wenmi_guo@163.com <wenmi_guo@163.com>
2019-11-01 08:49:34 +1300

    test scripts and results

---
+++ assignments/second/scripts/r.sh
@@ -0,0 +1,21 @@
+#mpic++ $1 -o $2
+
+for nodeNum in 1 2 3 4
+do
+    for pNum in 1 2 3 4 5 6 7 8
+    do 
+	echo "#PBS -j oe -o p$nodeNum-$pNum.stdout -l nodes=$nodeNum:ppn=$pNum -q pp
+ 	mpiexec -machinefile $PBS_NODEFILE /home/s19026416/parall/assignments/second/sort 64000" >>  p$nodeNum-$pNum.pbs
+ 	qsub p$nodeNum-$pNum.pbs
+    done
+done
+
+#qsub p$1-$2.pbs
+
+#sleep 15 &
+
+#echo "this is p$1-$2 --------------------" >> all.log
+#cat p$1-$2.stdout >> all.log
+
+#tail $3.stdout
+
